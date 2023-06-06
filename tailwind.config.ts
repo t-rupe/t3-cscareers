1253570e2ef64b2b265e7396b7531bee3789038b
Taylor Rupe <personal@trupe.info>
2023-06-06 10:10:40 -0700

    fixed gray text color issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,30 @@ export default {
           css: {
             color: theme('colors.white'),
           },
+          h1: {
+            color: theme('colors.white'),
+          },
+          h2: {
+            color: theme('colors.white'),
+          },
+          h3: {
+            color: theme('colors.white'),
+          },
+          h4: {
+            color: theme('colors.white'),
+          },
+          h5: {
+            color: theme('colors.white'),
+          },
+          h6: {
+            color: theme('colors.white'),
+          },
+          strong: {
+            color: theme('colors.white'),
+          },
+          li: {
+            color: theme('colors.white'),
+          },
         },
       }),
     },
