5d510ea2bb48ab68eb72da391e11b6dc3b2b29cc
Taylor Rupe <personal@trupe.info>
2023-06-27 09:29:49 -0700

    Updated sitemap for /degrees

--- src/pages/api/sitemap.xml.ts
+++ src/pages/api/sitemap.xml.ts
@@ -7,7 +7,7 @@ type Route = string;
 
 interface SanityPostSlug {
     slug: string;
-  }
+}
 
 async function getStaticRoutes(): Promise<Route[]> {
     return Promise.resolve( [
@@ -15,6 +15,7 @@ async function getStaticRoutes(): Promise<Route[]> {
         '/privacy-policy',
         '/terms-of-use',
         '/career',
+        '/degrees', // Added your new page
     ]);
 }
 
@@ -24,10 +25,18 @@ async function getDynamicCareerRoutes(): Promise<Route[]> {
     return data.map((item: SanityPostSlug) => `/career/${item.slug}`);
 }
 
+async function getDynamicDegreeRoutes(): Promise<Route[]> {
+    // Query Sanity for posts in the "Degrees" category with a defined main image
+    const query = groq`*[_type == "post" && category == "Degrees" && defined(mainImage)]{ "slug": slug.current }`;
+    const data: SanityPostSlug[] = await getClient().fetch(query);
+    return data.map((item: SanityPostSlug) => `/degrees/${item.slug}`); // Note the path change here
+}
+
 export default async function sitemap(req: NextApiRequest, res: NextApiResponse) {
     const staticRoutes = await getStaticRoutes();
     const dynamicCareerRoutes = await getDynamicCareerRoutes();
-    const routes = [...staticRoutes, ...dynamicCareerRoutes];
+    const dynamicDegreeRoutes = await getDynamicDegreeRoutes(); // Added your new dynamic route
+    const routes = [...staticRoutes, ...dynamicCareerRoutes, ...dynamicDegreeRoutes]; // Include the new dynamic routes
 
     const sitemap = create({ version: '1.0', encoding: 'UTF-8' }, {
         urlset: {
